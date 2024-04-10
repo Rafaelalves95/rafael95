47e9869c9118639553632a39713279651f217b82
Rafaelalves95 <119360474+Rafaelalves95@users.noreply.github.com>
2024-04-10 11:53:28 -0300

    Teste de conversão para string

--- aula-variavel/index.js
+++ aula-variavel/index.js
@@ -5,3 +5,9 @@ console.log("Hello world!")
 const nome = "Rafael"
 const idade = 29
 console.log("Meu nome é ", nome,"e tenho", idade,"anos de idade" )
+
+// Teste de conversão para string
+const idadenumero = 23
+const idadetexto = idadenumero.toString()
+console.log(typeof idadenumero)
+console.log(typeof idadetexto)
