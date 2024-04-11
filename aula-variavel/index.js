8a6168247e819752e6d84687fd03451c56cdff72
Rafaelalves95 <119360474+Rafaelalves95@users.noreply.github.com>
2024-04-11 10:30:19 -0300

    Editei arquivos index

--- aula-variavel/index.js
+++ aula-variavel/index.js
@@ -1,13 +1,23 @@
 //Início da aula:
-console.log("Hello world!")
+//console.log("Hello world!")
 
 //Minhas informações:
-const nome = "Rafael"
-const idade = 29
-console.log("Meu nome é ", nome,"e tenho", idade,"anos de idade" )
+//const nome = "Rafael"
+//const idade = 29
+//console.log("Meu nome é ", nome,"e tenho", idade,"anos de idade" )
 
 // Teste de conversão para string
-const idadenumero = 23
-const idadetexto = idadenumero.toString()
-console.log(typeof idadenumero)
-console.log(typeof idadetexto)
+//const idadenumero = 23
+//const idadetexto = idadenumero.toString()
+//console.log(typeof idadenumero)
+//console.log(typeof idadetexto)
+
+//Aula 2:
+
+//const primeironome = "Rafael" 
+//const segundonome = "Alves"
+//const idadenumero = "29"
+//const serEstudante = "Sim"
+//console.log("O seu nome e?", primeironome,"o seu sobrenome e?", segundonome, "idade?", idadenumero,"é estudante?", serEstudante)
+
+
---
+++ aulagit/template_javascript/template_javascript/index.js
@@ -0,0 +1,3 @@
+let nome = prompt("Qual é seu nome?")
+alert(`Seu nome e ${nome}`)
+
