b0ddc69364ed4cbda9e97754a5ff2ba912d4be10
Rafaelalves95 <119360474+Rafaelalves95@users.noreply.github.com>
2024-04-11 11:53:15 -0300

    Fim da aula de hoje

--- aulagit/template_javascript/template_javascript/index.js
+++ aulagit/template_javascript/template_javascript/index.js
@@ -1,3 +1,24 @@
+//let nome = prompt("Qual é seu nome?")
+//alert(`Seu nome e ${nome}`)
+
+//Exercício de Escrita de código:
+
+//let nome 
+//let idade 
+//console.log(typeof nome)
+//console.log(typeof idade)
+//Resposta: Pois não foi atribuido nenhum valor:
+
 let nome = prompt("Qual é seu nome?")
-alert(`Seu nome e ${nome}`)
+alert(`Seu nome é ${nome}`)
+let idade = prompt("Qual é a sua idade?")
+alert(`Sua idade é ${idade}`)
+//alert(`${nome}, ${idade}`) Teste
+alert(`Olá ${nome} , você tem ${idade} anos`)
+
+let roupa = prompt("Você está usando uma roupa azul hoje?")
+
+//fim da aula
+
+
 
