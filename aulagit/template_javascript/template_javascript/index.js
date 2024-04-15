2322191c78a8884e93468a7c358de734f6a6437e
Rafaelalves95 <119360474+Rafaelalves95@users.noreply.github.com>
2024-04-15 09:51:43 -0300

    Teste commit

--- aulagit/template_javascript/template_javascript/index.js
+++ aulagit/template_javascript/template_javascript/index.js
@@ -9,16 +9,40 @@
 //console.log(typeof idade)
 //Resposta: Pois não foi atribuido nenhum valor:
 
-let nome = prompt("Qual é seu nome?")
-alert(`Seu nome é ${nome}`)
-let idade = prompt("Qual é a sua idade?")
-alert(`Sua idade é ${idade}`)
+//let nome = prompt("Qual é seu nome?")
+//alert(`Seu nome é ${nome}`)
+//let idade = prompt("Qual é a sua idade?")
+//alert(`Sua idade é ${idade}`)
 //alert(`${nome}, ${idade}`) Teste
-alert(`Olá ${nome} , você tem ${idade} anos`)
+//alert(`Olá ${nome} , você tem ${idade} anos`)
 
-let roupa = prompt("Você está usando uma roupa azul hoje?")
+//let roupa = prompt("Você está usando uma roupa azul hoje?")
 
 //fim da aula
 
+//Início da aula 15/4
 
-
+//Recebeu variáveis
+let nome = prompt("Digite seu nome")
+let idade = prompt("Digite sua idade")
+//MENSAGEM: Digite as horas que você trabalha por dia:
+let horasTrabalho = prompt("Digite as horas que você trabalha, porfavor.")
+let salarioDia = prompt("Digite o seu salário diário:")
+//Verificando variáveis:
+console.log(typeof nome)
+console.log(typeof horasTrabalho)
+console.log(typeof salarioDia)
+//Convertendo o que é necessário:
+let horasTrabNumber = Number(horasTrabalho)
+let salDiaNumber = Number(salarioDia)
+//Processando valores:
+let valHoraNumber = horasTrabNumber/salDiaNumber
+//Conversão para não dar erro de NULL:
+let valorHora = String(valHoraNumber)
+const mensagem = "Seu valor hora é " + valorHora + "!!!"
+//Verificando variáveis
+console.log(typeof valorHora)
+console.log(typeof mensagem)
+console.log(mensagem)
+// Saída de valores para Front End
+alert(mensagem)
