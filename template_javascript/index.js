5abb8e333452b0e847b2884cd3be14d1b2a69d49
Rafaelalves95 <119360474+Rafaelalves95@users.noreply.github.com>
2024-04-17 12:05:14 -0300

    Finalizando aula

--- template_javascript/index.js
+++ template_javascript/index.js
@@ -1,6 +1,96 @@
-let primeiroNumero = Number(prompt("Digite um numero!"))
-let segundoNumero = Number(prompt("Digite outro numero!"))
+/*
+//1= Declarando variáveis:
+let idadeUsuario
+let idadeBestamigo
+let diferençaIdade
+let idadeMaior
+let diferençaConsole
 
-const soma = primeiroNumero + segundoNumero
+//2= Recebendo valores:
 
-alert(soma)
+idadeUsuario =  Number(prompt("Qual sua idade"))
+idadeBestamigo = Number(prompt("Qual idade do seu melhor amigo(a)?"))
+
+//3- Calculando valores:
+diferençaIdade = idadeUsuario - idadeBestamigo
+idadeMaior = idadeUsuario > idadeBestamigo
+diferençaConsole = idadeUsuario - idadeBestamigo
+
+
+//4- Resultado web:
+alert(diferençaIdade)
+alert(idadeMaior)
+alert(diferençaConsole)
+
+//5- Resultado console:
+console.log(diferençaIdade)
+console.log(idadeMaior)
+console.log(diferençaConsole)
+
+*/
+
+//Exercício 2:
+/*
+//1-Declarando variáveis:
+let numeroPar
+
+//2-Recebendo valores:
+
+//3- Calculando valores
+numeroPar = prompt("Digite um número par!")
+
+//4- Resultado web:
+alert(numeroPar)
+
+//5- Resultado concole:
+console.log((numeroPar%2)==0)
+//c) Você notou um padrão?  Sim, o resultado somente positivo e true.
+//d) O que acontece se o usuário inserir um número ímpar? Exibe false no console.
+
+
+//Exercício 3:
+
+
+//1- Declaração de variáveis:
+let idadeAnos
+let idadeMeses
+let idadeDias
+let idadeHoras
+
+//2- Recebendo Valores:
+idadeAnos = prompt("Digite sua idade em anos!")
+
+//3- Calcule Valores:
+idadeMeses = idadeAnos * 12
+idadeDias = idadeAnos * 365
+idadeHoras = idadeAnos * (365*24)
+
+//4- Mostra resultado web.
+alert(idadeAnos)
+alert(idadeMeses)
+alert(idadeDias)
+alert(idadeHoras)
+/*
+O primeiro numero é divisível pelo segundo? true
+    O segundo numero é divisível pelo primeiro? true
+    
+    obs: O true ou false vai depender dos números inseridos e do resultado das operações.
+    ```
+
+
+let numeroUm
+let numeroDois
+let numMaior
+let igual
+
+numeroUm = Number(prompt("Digite número1"))
+numeroDois = Number(prompt("Digite número2"))
+
+numMaior = numeroUm > numeroDois
+igual = numeroUm == numeroDois
+
+console.log(numeroUm)
+console.log(numeroDois)
+console.log((numeroUm%numeroDois)==0)
+console.log((numeroDois%numeroUm)==0)
+*/
