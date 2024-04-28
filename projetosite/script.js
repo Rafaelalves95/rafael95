52408c76d1a2aefb3ae0c6fbf159660afd0dd72f
Rafaelalves95 <rafaelalvesdesouza95@gmail.com>
2024-04-27 22:27:45 -0300

    Finalizei a primeira parte

---
+++ projetosite/script.js
@@ -0,0 +1,54 @@
+/*===== SISTEMA DE CADASTRO DE DOADORES DE SANGUE =====
+1. Cadastrar doador
+2. Listar doadores
+3. Buscar doador por tipo sanguíneo
+4. Buscar doador por data da última doação
+5. Sair
+Escolha uma opção:
+*/
+
+// Recebendo dados:
+
+//cadastroDoador
+
+const name = prompt("Qual seu nome?")
+const yearsOld = Number(prompt("Qual sua idade?"))
+const kg = prompt("Qual o seu peso?")
+const typeBlood = prompt("Qual seu tipo sanguíneo?")
+const lastDonation = prompt("Qual a data (dd/mm/aaaa) da sua última doação?")
+
+const dadosDoador = {
+    Nome: name,
+    Idade: yearsOld,
+    Peso: kg,
+    TipoSanguíneo: typeBlood,
+    lastDoação: lastDonation,
+}
+console.log(dadosDoador)
+
+
+
+
+
+/*
+const cachorro = {
+	nome: "Juca", 
+	idade: 3, 
+	raca: "SRD"
+}
+
+const gato = {...cachorro, nome: "Juba"}
+
+const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+console.log(cachorro)
+console.log(gato)
+console.log(tartaruga)
+/*
+console.log("------------------------")
+console.log("Listagem de Doadores:")
+console.log("----------------------------------------------------------------")
+console.log("Nome" | "Idade" | "Peso" | "Tipo Sanguineo" + "última Doação" )
+console.log("----------------------------------------------------------------")
+*/
+
